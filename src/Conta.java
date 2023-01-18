02d80bd0bf9730aa5edaf930d47908a4f52d8a3a
Josemar Nascimento <jsm_nascimento@hotmail.com>
2023-01-17 21:51:07 -0300

    update class

--- src/Conta.java
+++ src/Conta.java
@@ -1,15 +1,16 @@
 public class Conta {
-    double saldo;
-    int agencia;
-    int numero;
-    String titular;
+
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
 
     public void deposita(double valor) {
-        this.saldo += valor;
+        this.saldo = this.saldo + valor;
     }
 
     public boolean saca(double valor) {
-        if(this.saldo >= valor) {
+        if (this.saldo >= valor) {
             this.saldo -= valor;
             return true;
         } else {
@@ -18,11 +19,40 @@ public class Conta {
     }
 
     public boolean transfere(double valor, Conta destino) {
-        if(this.saldo >= valor) {
+        if (this.saldo >= valor) {
             this.saldo -= valor;
             destino.deposita(valor);
             return true;
         }
         return false;
     }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    public Cliente getTitular() {
+        return this.titular;
+    }
+
 }
--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -1,12 +1,12 @@
 public class TestaMetodo {
     public static void main(String[] args) {
         Conta contaDoPaulo = new Conta();
-        contaDoPaulo.saldo = 100;
+        contaDoPaulo.deposita(100);
         contaDoPaulo.deposita(50);
-        System.out.println(contaDoPaulo.saldo);
+        System.out.println(contaDoPaulo.getSaldo());
 
         boolean conseguiuRetirar = contaDoPaulo.saca(20);
-        System.out.println(contaDoPaulo.saldo);
+        System.out.println(contaDoPaulo.getSaldo());
         System.out.println(conseguiuRetirar);
 
         Conta contaDaMarcela = new Conta();
@@ -19,8 +19,10 @@ public class TestaMetodo {
 
         } else {
             System.out.println("faltou dinheiro");
+
         }
-        System.out.println(contaDaMarcela.saldo);
-        System.out.println(contaDoPaulo.saldo);
+        System.out.println(contaDaMarcela.getSaldo());
+        System.out.println(contaDoPaulo.getSaldo());
     }
+
 }
